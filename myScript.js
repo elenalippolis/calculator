fca2ef04c63bd7fe24bc41a211013ba497be5fd4
elenalippolis <56910933+elenalippolis@users.noreply.github.com>
2020-06-26 17:46:27 +0200

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,7 +1,5 @@
-function add(){
-  int x=0;
-  document.getElementById("demo").innerHTML=x+1;
-}
-function reset(){
-  document.getElementById("demo").innerHTML=0;
+function doNum(n){
+
+  	document.getElementById("screen").value="n";
+
 }
